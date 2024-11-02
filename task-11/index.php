27088b31edcc35e4eee9b9f177881f92e25ea962
Ali Hamza <hamzamashooq492@gmail.com>
2024-11-02 12:03:08 +0500

    Print & Echo

---
+++ task-11/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $greeting = "Hello, World!";
+
+    // Display  using echo
+    echo"By using echo " ,$greeting, "<br>"; 
+    
+    // Display using print
+    print'By using print ';
+    print $greeting; 
+    ?>
+</body>
+
+</html>
